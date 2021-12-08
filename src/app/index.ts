632eb894789550e689774791bb6c02900c15cb7a
Karl <ollylut@gmail.com>
2021-12-08 09:44:04 +0300

    Staging (#4)
    * fix: readme
    
    * fix: cors
    
    * fix: debug

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,7 +2,7 @@ import express, { json } from 'express'
 import cookie from 'cookie-parser'
 import 'express-async-errors'
 
-import { queryParser, logger, error } from './middlewares'
+import { queryParser, logger, error, cors } from './middlewares'
 import { router } from './router'
 
 export const app = express()
@@ -11,6 +11,7 @@ app.set('trust proxy', true)
 app.use(json())
 app.use(cookie())
 app.set('query parser', queryParser)
+app.use(cors)
 app.use(logger)
 app.use(router)
 app.use(error)
