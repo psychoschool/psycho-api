102cc49bf33b9f84949fa096445d8be9dcf10a37
Denis Aleksandrov <ollylut@gmail.com>
2021-12-20 09:04:05 +0300

    fix: add update courses types

--- src/app/resources/schemas/course/course.normalizer.ts
+++ src/app/resources/schemas/course/course.normalizer.ts
@@ -1,3 +1,12 @@
 import { Course, CourseResponse } from 'app/resources/types'
 
-export const normalizeCourse = (course: Course): CourseResponse => course
+export const normalizeCourse = (course: Course): CourseResponse => ({
+    id: course.id,
+    title: course.title,
+    image: course.image,
+    isFree: course.isFree,
+    description: course.description,
+    author: course.author,
+    sections: course.sections,
+    paidPlans: course.paidPlans
+})
