34d1972b0a9d650fc8be936629a239cb337073a3
Denis Aleksandrov <ollylut@gmail.com>
2022-01-06 22:41:28 +0300

    feat: update course schema - add duration prop

--- src/app/resources/schemas/course/course.schema.ts
+++ src/app/resources/schemas/course/course.schema.ts
@@ -7,8 +7,22 @@ const LectureSchema = new Schema<Lecture>({
     video: {
         provider: String,
         videoId: String,
-        videoUrl: String
+        videoUrl: String,
+        duration: Number
     }
+})
+
+const SectionSchema = new Schema<CourseDoc['sections'][number]>({
+    title: String,
+    lectures: [LectureSchema]
+})
+
+SectionSchema.virtual('duration').get(function (this: CourseDoc['sections'][number]) {
+    return this.lectures.reduce((res, lec) => {
+        const duration = lec.video?.duration ?? 0
+
+        return res + duration
+    }, 0)
 })
 
 const CourseSchema = new Schema<CourseDoc>({
@@ -17,7 +31,7 @@ const CourseSchema = new Schema<CourseDoc>({
     url: { type: String, unique: true },
     image: { type: String, required: [true, 'image is required'] },
     author: { type: 'ObjectId', ref: 'users', required: [true, 'author is required'] },
-    sections: [{ title: String, lectures: [LectureSchema] }],
+    sections: [SectionSchema],
     skills: [String],
     price: { cost: Number, promoCost: Number },
     isFree: { type: Boolean, required: [true, 'isFree is required field'] }
@@ -25,6 +39,10 @@ const CourseSchema = new Schema<CourseDoc>({
 
 CourseSchema.virtual('lecCount').get(function (this: CourseDoc) {
     return this.sections.reduce((res, sec) => res + sec.lectures.length, 0)
+})
+
+CourseSchema.virtual('duration').get(function (this: CourseDoc) {
+    return this.sections.reduce((res, sec) => res + sec.duration, 0)
 })
 
 export const CourseModel = model('courses', CourseSchema)
