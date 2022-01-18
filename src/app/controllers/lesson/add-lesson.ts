949b9b8213c3f7a4d7bf68594d1a8f066c38eeb0
Karl <ollylut@gmail.com>
2022-01-18 08:41:31 +0300

    Staging (#18)
    * feat: mail notification (#16)
    
    * feat: mail notification

--- src/app/controllers/lesson/add-lesson.ts
+++ src/app/controllers/lesson/add-lesson.ts
@@ -1,11 +1,23 @@
 import { NextFunction, Request, Response } from 'express'
 import * as lessonResource from 'app/resources/api/lesson.resource'
+import { sendEmail } from 'src/utils/mail'
 
 export const addLesson = (req: Request, res: Response, next: NextFunction) => {
     const { course, user, url, price } = req.body
     // todo: add validation
     lessonResource
         .addLesson(course, user, url, price)
+        .then(lesson => {
+            if (lesson) {
+                sendEmail({
+                    to: lesson.user.email,
+                    subject: 'Вы зачислены!',
+                    template: 'start-course',
+                    context: { user: lesson.user, course: lesson.course, date: new Date().toLocaleDateString() }
+                })
+            }
+            return lesson
+        })
         .then(lesson => res.json({ result: lesson }))
         .catch(next)
 }
