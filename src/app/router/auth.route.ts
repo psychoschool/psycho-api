02739865108da1398c82cecf584d58b4e7ca4a82
Denis Aleksandrov <ollylut@gmail.com>
2022-01-25 17:58:36 +0300

    feat: password reset - add forgot endpoint

--- src/app/router/auth.route.ts
+++ src/app/router/auth.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { signUp, signIn, signOut } from 'app/controllers'
+import { signUp, signIn, signOut, forgot } from 'app/controllers'
 
 export const authRoutes = (router: Router) => {
     router.post('/api/v1/signup', signUp)
     router.post('/api/v1/login', signIn)
     router.post('/api/v1/logout', signOut)
+    router.post('/api/v1/forgot', forgot)
 }
